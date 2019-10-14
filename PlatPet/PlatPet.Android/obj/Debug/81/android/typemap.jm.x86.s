533ffc72160f8f8d436bfb76257eabf8a941201d
viniciuslira <viniciuslira@SATURNO>
2019-10-14 04:22:29 +0000

    Confirm

---
+++ PlatPet/PlatPet.Android/obj/Debug/81/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: d0c2412377ca8152e0326a4cf89675d858cad907 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	221
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 51273
+	.include	"typemap.jm.inc"
---
+++ PlatPet/PlatPet.Android/obj/Debug/81/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 517549fe4c739920ee0391ba7115df82fcd61633 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	239
+	/* entry-length */
+	.word	232
+	/* value-offset */
+	.word	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 55449
+	.include	"typemap.mj.inc"
